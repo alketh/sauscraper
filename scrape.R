63199ede90e82b9beb835fe1f247bd71b7b24408
Alexander Keth <alexander.keth@posteo.net>
2025-08-19 21:05:56 +0200

    Add db support.

--- scrape.R
+++ scrape.R
@@ -1,8 +1,15 @@
 library(rvest)
 library(stringr)
 library(tibble)
+library(purrr)
+library(lubridate)
+library(DBI)
+library(RPostgres)
+library(openssl)
 
-url <- "https://www.salzwasserunion.de/de/sau-in-aktion/veranstaltungen/cat.listevents"
+source("utils.R")
+
+url <- Sys.getenv("RVAR_URL")
 
 content <- read_html(url)
 
@@ -22,3 +29,58 @@ time <- str_split_fixed(raw[, 2], pattern = " - ", n = 2)
 # lets keep it simple
 out <- tibble(time = str_trim(raw[, 2]), event = str_trim(raw[, 3]))
 out
+
+# lets make it fancy
+# x <- time[1, 1]
+
+out$from_weekday <- map_chr(time[, 1], parse_weekday)
+out$from_date <- map_chr(time[, 1], parse_date)
+out$from_time <- map_chr(time[, 1], parse_time)
+
+out$to_weekday <- map_chr(time[, 2], parse_weekday)
+out$to_date <- map_chr(time[, 2], parse_date)
+out$to_time <- map_chr(time[, 2], parse_time)
+
+missing_date <- is.na(out$to_date)
+
+out$to_weekday[missing_date] <- out$from_weekday[missing_date]
+out$to_date[missing_date] <- out$from_date[missing_date]
+
+out$from_date <- as.Date(out$from_date, format = "%d.%m.%y")
+out$to_date <- as.Date(out$to_date, format = "%d.%m.%y")
+
+out$time <- NULL
+
+# Add meta data
+out$id <- md5(apply(out, 1, paste, collapse = "|"))
+out$created_at <- Sys.time()
+
+# fix column order
+out <- out[, c("id", "from_date", "from_time", "from_weekday", "to_date", "to_time", "to_weekday", "event", "created_at")]
+
+# Add a testevent
+out <- create_testevent()
+
+con <- con_sau()
+
+dbExecute(con, "DROP TABLE IF EXISTS temp_events")
+dbWriteTable(con, name = "temp_events", value = out)
+
+# Recreate from scratch
+# dbExecute(con, "DROP TABLE IF EXISTS events")
+# dbWriteTable(con, name = "events", value = out)
+
+# Check for new entries!
+ids <- dbGetQuery(con, "select distinct id from events")
+
+new <- out$id[!out$id %in% ids$id]
+
+check <- length(new) > 0
+
+# Add new events and report
+if (check) {
+  add <- out[out$id %in% new, ]
+  nn <- dbAppendTable(con, name = "events", value = add)
+}
+
+dbDisconnect(con)
---
+++ utils.R
@@ -0,0 +1,66 @@
+parse_time <- function(x) {
+  out <- str_extract_all(string = x, pattern = "[0-9][0-9]:[0-9][0-9]")
+  out <- out[[1]]
+  
+  if (length(out) == 2) out <- paste(out, collapse = "-")
+  if (length(out) == 0) out <- NA
+  
+  out
+}
+
+parse_weekday <- function(x) {
+  days <- c("Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag")
+  out <- str_extract(x, pattern = days)
+  out <- as.character(na.omit(out))
+  
+  if (length(out) == 0) out <- NA
+  
+  out
+}
+
+parse_date <- function(x) {
+  months <- c("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember")
+  months_digit <- gsub(" ", "0", format(1:12, digits = 2))
+  months_digit <- paste0(months_digit, ".")
+  
+  out <- str_replace(x, pattern = paste0(" ", months, " "), replacement = months_digit)
+  nn <- nchar(out)
+  out <- out[nn == min(nn)]
+  
+  res <- str_extract(out, pattern = "[0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9][0-9][0-9]")
+  
+  if (length(res) != 1) res <- NA
+  
+  res
+}
+
+con_sau <- function() {
+  DBI::dbConnect(
+    drv = RPostgres::Postgres(), 
+    user = Sys.getenv("RVAR_DBUSER"), 
+    password = Sys.getenv("RVAR_DBPASSWORD"), 
+    host = Sys.getenv("RVAR_DBHOST"), 
+    port = as.integer(Sys.getenv("RVAR_DBPORT")), 
+    dbname = Sys.getenv("RVAR_DBNAME")
+  )
+}
+
+create_testevent <- function() {
+  fd <- Sys.Date()
+  miss <- NA
+  ev <- "testevent"
+  
+  id <- md5(paste(fd, miss, miss, fd, miss, miss, ev, collapse = "|"))
+  
+  tibble(
+    id = id,
+    from_date = fd,
+    from_time = miss,
+    from_weekday = miss,
+    to_date = fd,
+    to_time = miss,
+    to_weekday = miss,
+    event = ev,
+    created_at = Sys.time()
+  )
+}
